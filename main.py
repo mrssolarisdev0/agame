12a2aa030b3d35cc352cd752352cfe2686d7960e
mrssolarisdev0 <105184910+mrssolarisdev0@users.noreply.github.com>
2022-05-08 23:57:32 -0300

    É só rodar isso com a mensagem e chave certa

---
+++ main.py
@@ -0,0 +1,17 @@
+from cryptography.fernet import Fernet
+
+def encrypt_this(message, key):
+    fernet = Fernet(key)
+    return fernet.encrypt(message.encode())
+
+def decrypt_this(message, key):
+    fernet = Fernet(key)
+    return fernet.decrypt(message).decode()
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    key = b'h0WBu417FzdD2pFcSz-tq2I5hVPopuKfFlTN_WGN5UQ='
+    message = "A mensagem aqui"
+    decrypted_message = decrypt_this(message, key)
+    print('Mensagem decriptada:', decrypted_message)
